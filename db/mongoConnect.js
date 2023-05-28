c4d4cfc6692a19436392451b92a02557b002e416
djr3231 <dajroth14@gmail.com>
2023-05-28 10:42:07 +0300

    efe

--- db/mongoConnect.js
+++ db/mongoConnect.js
@@ -6,7 +6,6 @@ main().catch(err => console.log(err));
 async function main() {
   await mongoose.connect(`mongodb://127.0.0.1:27017/Half_project`);
   console.log("mongo connect Half_project");
-  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
 
 

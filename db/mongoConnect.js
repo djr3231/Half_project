ad04347cb214d08e94ae65055cfc4e14474e2d0f
djr3231 <dajroth14@gmail.com>
2023-05-28 09:59:32 +0300

    efe

--- db/mongoConnect.js
+++ db/mongoConnect.js
@@ -4,8 +4,8 @@ const mongoose = require('mongoose');
 main().catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect(`mongodb+srv://${config.USER_DB}:${config.PASS_DB}@davdata.it0ecvp.mongodb.net/idf8`);
-  console.log("mongo connect idf8 atlas");
+  await mongoose.connect(`mongodb://localhost:27017/idf8`);
+  console.log("mongo connect idf8");
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
 

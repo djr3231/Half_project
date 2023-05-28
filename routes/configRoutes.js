6dd641f1b28d64a2f567ffeb3c7d88964771f81e
djr3231 <dajroth14@gmail.com>
2023-05-28 11:45:46 +0300

    wrwhy

--- routes/configRoutes.js
+++ routes/configRoutes.js
@@ -1,11 +1,9 @@
 const indexR = require("./index");
 const usersR = require("./users");
-const categoriesR = require("./categories");
 const toysR = require("./toys");
 
 exports.routesInit = (app) => {
   app.use("/",indexR);
   app.use("/users",usersR);
-  app.use("/categories",categoriesR);
   app.use("/toys",toysR);
 }

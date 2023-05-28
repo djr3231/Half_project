35cead92debabd77ce7fb2e16d3cb3f07a30f98e
djr3231 <dajroth14@gmail.com>
2023-05-28 11:19:43 +0300

    ev

--- config/secret.js
+++ config/secret.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 console.log(process.env.TOKEN_SECRET);
 exports.config = {
-    PASS_DB:process.env.PASS_DB,
-    USER_DB:process.env.USER_DB,
+    // PASS_DB:process.env.PASS_DB,
+    // USER_DB:process.env.USER_DB,
     TOKEN_SECRET:process.env.TOKEN_SECRET
   }

b3160d45dc44487a500c891fa1152af4e25e14b4
djr3231 <dajroth14@gmail.com>
2023-05-30 15:52:37 +0300

    dd

--- db/mongoConnect.js
+++ db/mongoConnect.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 main().catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect(`mongodb+srv://${config.PASS_DB}:${config.USER_DB}@davdata.it0ecvp.mongodb.net/idf8`);
+  await mongoose.connect(`mongodb+srv://${config.USER_DB}:${config.PASS_DB}@davdata.it0ecvp.mongodb.net/idf8`);
   console.log("mongo connect Half_project");
 }
 
